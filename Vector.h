41f7df87208150224691c51de7f6362656c91747
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-30 17:21:12 +0300

    Vector

---
+++ Vector.h
@@ -0,0 +1,58 @@
+//
+// Created by Lapshun Tetiana on 30.06.2025.
+//
+
+#ifndef P43_T22_VECTOR_VECTOR_H
+#define P43_T22_VECTOR_VECTOR_H
+
+#include <iostream>
+using  namespace std;
+
+template <typename Type>
+class Vector{
+    Type* arr;
+    int size;
+
+public:
+    Vector(){
+        arr = nullptr;
+        size = 0;
+    }
+
+    Vector(int _size, Type value){ //_size = 4, value = 7.8
+        size = _size;
+        arr = new Type[size]; // [0, 0, 0, 0]
+
+        for (int i = 0; i < size; ++i) {
+            arr[i] = value; //[7.8, 7.8, 7.8, 7.8]
+        }
+    }
+
+    ~Vector(){
+        if( size > 0) delete[] arr;
+        arr = nullptr;
+        size = 0;
+    }
+
+    void showInfo()const{
+        if( size == 0) cout << "Array is empty!\n";
+        else{
+            for (int i = 0; i < size; ++i) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif //P43_T22_VECTOR_VECTOR_H
