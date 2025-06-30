fa386f2b9736980e0d442dd032080ed32c904c29
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-30 17:50:13 +0300

    Vector

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 
 int main() {
     Vector<int> a; //size = 0
+    a.push_back(-4);
+    a.push_back(-9);
+
     Vector<int> b(3, 8); //[8, 8, 8]
+    b.push_back(89);
+    b.push_back(56);
 
     Vector<char> c(4, 'a');
+    c.push_back('h');
+    c.push_back('k');
 
 
     a.showInfo();
